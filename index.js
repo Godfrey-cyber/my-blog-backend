510f78cc70d3d1f6f063e085db7aa711fb8dcd78
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2023-08-21 07:10:07 +0300

    changed the url in th e index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import postRoutes from "./routes/posts.js"
 import categoryRoutes from "./routes/categories.js"
 
 const app = express()
-app.use(cors({ credentials: true, origin: "http://localhost:5173"} ))
+app.use(cors({ credentials: true, origin: "http://lhttps://my-blog-frontend-kappa.vercel.app"} ))
 app.use(express.json())
 app.use(express.json({limit: '25mb'}));
 // app.use(express.urlencoded({limit: '25mb'}));
