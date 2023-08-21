e736e9c07afeb0cfa8f323be537848f747d576ab
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2023-08-21 09:20:51 +0300

    url update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ import categoryRoutes from "./routes/categories.js"
 const app = express()
 app.use(cors({ credentials: true, origin: {
 	"https://my-blog-frontend-kappa.vercel.app",
-	"https://my-blog-frontend-kappa.vercel.app/register"
+	"https://my-blog-backend-t19h.onrender.com/users/register"
 }} ))
 app.use(express.json())
 app.use(express.json({limit: '25mb'}));
